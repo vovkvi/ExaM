ad76248818454496d1ccc9ac37d82bde1f822ef4
Vitalii Vovk <99590889+vovkvi@users.noreply.github.com>
2022-09-17 21:41:22 +0300

    Update and rename pages.js to config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,55 +3,66 @@ var pages = [
         "ot" : [
             "ОТ",
             "Правила по охране труда",
-            "ri-alert-fill"
+            "ri-alert-fill",
+            ["ot.js"]
             ],
         "pprp" : [
             "ППРП",
             "Правила работы с персоналом",
-            "ri-group-fill"
+            "ri-group-fill",
+            ["pprp.js"]
         ],
         "pte" : [
             "ПТЭ",
             "Правила технической эксплуатации",
-            "ri-tools-fill"
+            "ri-tools-fill",
+            ["pte.js"]
             ],
     },
     {
         "eb2" : [
             "ЭБ-II",
             "Электробезопасность (группа II)",
-            "ri-flashlight-fill"
+            "ri-flashlight-fill",
+            ["eb2.js", "eb.js"]
             ],
         "eb3" : [
             "ЭБ-III",
             "Электробезопасность (группа III)",
-            "ri-flashlight-fill"], 
+            "ri-flashlight-fill",
+            ["eb3.js", "eb.js"]
+            ], 
         "eb4" : [
             "ЭБ-IV",
             "Электробезопасность (группа IV)",
-            "ri-flashlight-fill"
+            "ri-flashlight-fill",
+            ["eb4.js", "eb.js"]
             ],
         "eb5" : [
             "ЭБ-V",
             "Электробезопасность (группа V)",
-            "ri-flashlight-fill"
+            "ri-flashlight-fill",
+            ["eb5.js", "eb.js"]
             ],
     },
     {
         "pb" : [
             "ПБ",
             "Правила пожарной безопасности",
-            "ri-fire-fill"
+            "ri-fire-fill",
+            ["pb.js"]
             ],
         "nds" : [
             "НДС",
             "Нарядно-допускная система",
-            "ri-edit-2-fill"
+            "ri-edit-2-fill",
+            ["nds.js"]
             ],
         "op" : [
             "ОП",
             "Правила производства оперативных переключений",
-            "ri-scan-fill"
+            "ri-scan-fill",
+            ["op.js"]
             ],
     },
 ];
